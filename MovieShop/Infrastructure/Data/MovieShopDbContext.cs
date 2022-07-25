818391f46840517b2c6e17d0808a1cb614d6c138
jonahq <jonahqgate@gmail.com>
2022-07-25 14:53:21 -0400

    h

---
+++ MovieShop/Infrastructure/Data/MovieShopDbContext.cs
@@ -0,0 +1,21 @@
+﻿using ApplicationCore.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data
+{
+    public class MovieShopDbContext : DbContext
+    {
+        public MovieShopDbContext(DbContextOptions<MovieShopDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Movie> Movies { get; set; }
+
+    }
+}
