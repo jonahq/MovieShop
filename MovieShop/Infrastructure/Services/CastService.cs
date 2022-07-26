85b5afc6e5105701f4862d346c50a98f55df580a
jonahq <jonahqgate@gmail.com>
2022-07-25 23:07:29 -0400

    h

--- MovieShop/Infrastructure/Services/CastService.cs
+++ MovieShop/Infrastructure/Services/CastService.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using ApplicationCore.Models;
+using ApplicationCore.RepositoryContracts;
+using ApplicationCore.ServiceContracts;
+using Infrastructure.Repositories;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,25 @@ using System.Threading.Tasks;
 
 namespace Infrastructure.Services
 {
-    internal class CastService
+    public class CastService : ICastService
     {
+        private readonly ICastRepository _castRepository;
+        public CastService(ICastRepository movieRepository)
+        {
+            _castRepository = movieRepository;
+        }
+
+        public Task<List<CastModel>> GetCastDetails(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<CastModel>> GetCastDetails()
+        {
+            throw new NotImplementedException();
+        }
+
+
     }
 }
+
